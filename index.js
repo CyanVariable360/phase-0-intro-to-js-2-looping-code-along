c85c308d5a387224b4ed7ed181546f61a1d3e683
Matthew Brighton Smith <brightonsmith@gmail.com>
2024-08-08 10:56:17 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const names = ["Guadalupe","Ollie","Aki"];
+const eventName = "surprise"
+
+function writeCards(names, eventName) {
+    const letters = [];
+    for(let i = 0; i < names.length; i++)
+    {
+        letters.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return letters;
+}
+
+function countDown(num = 10)
+{
+    while(num >= 0)
+    {
+        console.log(num--);
+    }
+    return num;
+}
+
+
+
